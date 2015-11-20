367f80ca8d4a5834901f57710b593c26f0191630
kj-schmidt <karljohan_schmidt@yahoo.dk>
2015-11-20 11:34:19 +0100

    Add alpha cropped

--- Prototype/Eclipse/.ino.cpp
+++ Prototype/Eclipse/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-11-20 10:42:21
+//This file has been generated on 2015-11-20 11:26:50
 
 #include "Arduino.h"
 #include "Arduino.h"
