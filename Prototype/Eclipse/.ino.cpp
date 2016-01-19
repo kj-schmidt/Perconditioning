adb562faf9eb56d6c482679c027d0340336169dd
kj-schmidt <karljohan_schmidt@yahoo.dk>
2016-01-19 22:18:57 +0100

    Latest changes

--- Prototype/Eclipse/.ino.cpp
+++ Prototype/Eclipse/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-12-07 13:58:38
+//This file has been generated on 2016-01-18 18:42:56
 
 #include "Arduino.h"
 #include "Arduino.h"
--- Prototype/Eclipse/Utilities.cpp
+++ Prototype/Eclipse/Utilities.cpp
@@ -9,15 +9,20 @@
 
 double Utilities::rawToMmHg(unsigned short rawPressure)
 {
-	double offset = 88;
+	double offset = 85; //88 med gammel sensor
 	double coerficient = 0.408;
 
-	return (rawPressure-offset)*coerficient;
+	double conversion = (rawPressure-offset)*coerficient;
+
+	if(conversion < 0)
+		conversion = 0;
+
+	return conversion;
 };
 
 double Utilities::mmHgToRaw(unsigned short mmHgPressure)
 {
-	double offset = 88;
+	double offset = 85;
 	double coerficient = 0.408;
 
 	return mmHgPressure/coerficient+offset;
